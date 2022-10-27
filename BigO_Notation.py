fc8ed8e90d035c887457c4998052d70ed32e85f4
maclamour <maclamour@gmail.com>
2022-10-27 06:15:00 -0400

    algos

---
+++ BigO_Notation.py
@@ -0,0 +1,16 @@
+"""
+Complexity Analysis
+
+Time complexity: How much time does it take to run completley; Algorithm runs fast,
+better time complexity
+Space complexity: How much extra space does it require in the process; algorithm takes less
+space, better space complexity
+
+Big factors are is it readable and scalable
+
+"""
+
+"""
+
+
+"""
